c5ccbd5d8eaf41424834b304663b1e04d1d654f5
Baruc229 <schallom229@gmail.com>
2026-09-02 19:41:39 +0100

    test: add pre-commit hook (ESLint --fix + Vitest), auto-fix on commit, prettier via eslint-config-prettier

--- app/eslint.config.mjs
+++ app/eslint.config.mjs
@@ -1,11 +1,13 @@
 import coreWebVitals from "eslint-config-next/core-web-vitals";
 import typescript from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier";
 
 const eslintConfig = [
   ...[].concat(coreWebVitals),
   ...[].concat(typescript),
+  ...[].concat(prettier),
   {
-    ignores: [".next/**", "out/**", "build/**", "src/generated/**"],
+    ignores: [".next/**", "out/**", "build/**", "src/generated/**", "e2e/**"],
   },
 ];
 
